e72936812b06143d11677b3afad9e62711e42d1b
dafa <dafadhiyaul24@gmail.com>
2023-09-24 09:46:58 +0700

    img

--- src/view/Body/body.js
+++ src/view/Body/body.js
@@ -11,13 +11,14 @@ function Body() {
   const [limit, setLimit] = useState(10);
   const userSearch = UserSearch();
   const { dataSearch } = userSearch;
-  console.log(dataSearch);
 
   const arr = [];
+  const api_key =
+    "live_MA3BQI7c6Uz03lYO6wmN4WwRk4Pa63o092vNbLRGEnfYLxq3tWHR3Q3KzVyHw0Zg";
 
   useEffect(() => {
     setData(dataSearch);
-    axios.get(`breeds?limit=${limit}`).then((res) => {
+    axios.get(`breeds?limit=${limit}&api_key=${api_key}`).then((res) => {
       setData(res.data);
     });
   }, []);
@@ -25,11 +26,12 @@ function Body() {
   const moreData = () => {
     setTimeout(() => {
       setLimit(limit + 10);
-      axios.get(`breeds?limit=${limit}`).then((res) => {
+      axios.get(`breeds?limit=${limit}&api_key=${api_key}`).then((res) => {
         setData(res.data);
       });
     }, 1000);
   };
+  console.log(data);
   return (
     <div className="conBody">
       <InfiniteScroll
@@ -42,7 +44,11 @@ function Body() {
           {data.map((item, index) => (
             <Col style={{ marginBottom: 10 }} key={index}>
               <Card>
-                <Card.Img variant="top" src="?" />
+                <Card.Img
+                  variant="top"
+                  src={item.image.url}
+                  style={{ objectFit: "contain", width: "100%", height: 200 }}
+                />
                 <Card.Body>
                   <Card.Title>{item.name}</Card.Title>
                   <Button
